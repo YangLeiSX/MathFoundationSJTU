68692284f14a03172964e27d856835d69e2a7c5f
LeoYang <YangLeiSX@sjtu.edu.cn>
2019-05-21 20:30:26 +0800

    add function to find prime root and function to compute multiple with order

--- MathFoundation/Chapter5.hpp
+++ MathFoundation/Chapter5.hpp
@@ -10,6 +10,7 @@
 #define Chapter5_hpp
 
 #include "MathFoundation.hpp"
+#include <map>
 
 //计算模m简化剩余系的指数
 void findOrder(const int &m){
@@ -67,10 +68,12 @@ void opeOrder(const int &m, const int &a){
 }
 
 //计算模m的原根
-void findPrimRoot(const int &m){
+//返回原根的数量
+int findPrimeRoot(const int &m){
     int answer;//计算结果
     int e;//指数
     int phi = EularFunction(m);
+    int count = 0;
     
     //选择底数
     for(int a = 1;a < m;a++){
@@ -86,12 +89,92 @@ void findPrimRoot(const int &m){
             }
             answer = opeMod(a, e, m);
             if(answer == 1 ){
-                if(e == phi)
+                if(e == phi){
                     std::cout << "," << a ;
+                    count ++;
+                }
                 break;
             }
             e++;
         }
     }
+    
+    return count;
+}
+
+//计算模m最小原根
+int minPrimeRoot(const int &p){
+    int answer;//计算结果
+    int e;//指数
+    int phi = EularFunction(p);
+    bool found = false;
+    
+    int g = 2;
+    while( g++ < p){
+        if(MaxCommonFactor(g, p) != 1)
+            continue;
+        e = 1;
+        while(e++ < p){
+            if( phi % e != 0)
+                continue;
+            answer = opeMod(g, e, p);
+            found = (answer == 1)&&(e == phi);
+            if(found)break;
+        }
+        if(found)break;
+    }
+    return g;
+}
+
+//计算模p^a的原根,实际上为计算p的一个原根
+//返回模p^a的最小原根
+int findPAPrimeRoot(const int &p,const int a = 1,bool printAllRoot = false){
+    //先求模p的最小原根
+    int g = minPrimeRoot(p);
+    
+    if( opeMod(g,p-1,p) == 1)
+        std::cout << g << " is prime root mod " << p << "^" << a << ".\n";
+    if( opeMod(g+p, p-1, p) == 1)
+        std::cout << g+p << " is prime root mod " << p << "^" << a << ".\n";
+    
+    if(printAllRoot){//显示所有的原根
+        int phix = EularFunction(pow(p,a));
+        int d = 2;
+        std::cout << "all root is as follow:\n";
+        std::cout << g;
+        while(d++ < phix){
+            if(MaxCommonFactor(d, phix) != 1)
+                continue;
+            std::cout << "," << opeMod(g, d, pow(p,a)) ;
+        }
+        std::cout << std::endl;
+    }
+    return g;
+}
+
+//使用原根简化乘法运算
+int multiWithPR(const int &mod,const int &left,const int &right){
+    std::map<int,int> table;
+    std::map<int,int>::iterator itr;
+    //数据初始化
+    int g = minPrimeRoot(mod);
+    int answer = 0;
+    int phi = EularFunction(mod);
+    
+    //建立指数表
+    int i = 0;
+    while(i++ < phi){
+        table.insert(std::pair<int, int>(opeMod(g,i,mod),i));
+    }
+    //查找指数
+    int lefti,righti;
+    itr = table.find(left);
+    lefti = itr->second;
+    itr = table.find(right);
+    righti = itr->second;
+    //从指数计算得到结果
+    answer = opeMod(g,(lefti + righti) % phi , mod);
+    
+    return answer;
 }
 #endif /* Chapter5_hpp */
